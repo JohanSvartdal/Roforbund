d8478891a03da952d76d208dadf84cbacbab63f3
Christine <christinee@uia.no>
2020-10-23 12:28:34 +0200

    Lagt til MinProfil i BrukerDash

--- src/main/java/servlets/MinProfil.java
+++ src/main/java/servlets/MinProfil.java
@@ -5,13 +5,14 @@ import tools.repository.DatabaseReader;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-@WebServlet(name= "MinProfil")
+@WebServlet(name= "MinProfil", urlPatterns = {"/UtoverDash/MinProfil/"})
 
 public class MinProfil extends AbstractAppServlet {
     @Override
@@ -24,28 +25,51 @@ public class MinProfil extends AbstractAppServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        ArrayList<Integer> utoverId = DatabaseReader.getAllEntries("roforbund.tester", "Test_id");
-        ArrayList<Test> TestListe = new ArrayList<>();
-
-        for (int i = 0; i < utoverId.size(); i++ ) {
-            Test current = new Test();
-
-            Integer test_id = DatabaseReader.getInt("roforbund.tester", "Utover_id", utoverId.get(i), "Test_id");
-            Integer klubb_id = DatabaseReader.getInt("roforbund.tester", "Utover_id", utoverId.get(i), "Klubb_id");
-            String dato = DatabaseReader.getString("roforbund.tester", "Utover_id", utoverId.get(i), "Dato");
-            Integer utover_id = DatabaseReader.getInt("roforbund.tester", "Utover_id", utoverId.get(i), "Trener_id");
-            Integer godkjent = DatabaseReader.getInt("roforbund.tester", "Utover_id", utoverId.get(i), "Godkjent");
-
-            current.setTestListe(test_id + " " + klubb_id + " " + dato + " " + utover_id + " " + godkjent);
-
-            ArrayList<Integer> testIds = DatabaseReader.getListOfIds("roforbund.tester", "utover_id", utoverId.get(i), "Test_id");
-            current.setAntallTester(testIds.size());
-
-
-            TestListe.add(current);
+        Cookie cklist[] = request.getCookies();
+        int UID = -1;
+        for (Cookie currentCookie:cklist) {
+            if(currentCookie.getName().equals("UID")) {
+                System.out.println(UID);
+                UID = Integer.parseInt(currentCookie.getValue());
+            }
         }
-        request.setAttribute("TestListe", TestListe);
-        RequestDispatcher rq = request.getRequestDispatcher("../Utoverdash/index.jsp");
+
+        if (UID == -1) {
+            System.out.println("User not found");
+        }
+
+        Integer fodseldato = DatabaseReader.getInt("roforbund.bruker", "Bruker_id", UID, "Fodseldato");
+        String epost = DatabaseReader.getString("roforbund.bruker", "Bruker_id", UID, "Epost");
+        Integer tlf = DatabaseReader.getInt("roforbund.bruker", "Bruker_id", UID, "Tlf");
+
+        String adresseID = DatabaseReader.getString("roforbund.bruker", "Bruker_id", UID, "Adresse_id");
+        String gatenavn = DatabaseReader.getString("roforbund.adresser", "Adresse_id", adresseID, "Gatenavn");
+        String husnummer = DatabaseReader.getString("roforbund.adresser", "Adresse_id", adresseID, "Husnummer");
+
+        Integer postnummer = DatabaseReader.getInt("roforbund.adresser", "Adresser_id", adresseID, "Postnummer");
+        String poststed = DatabaseReader.getString("roforbund.postnummer", "Postnummer", postnummer, "Poststed");
+
+
+        Integer hoyde = DatabaseReader.getInt("roforbund.bruker", "Bruker_id", UID, "Hoyde");
+        Integer vekt = DatabaseReader.getInt("roforbund.bruker", "Bruker_id", UID, "Fodseldato");
+        Integer rolle = DatabaseReader.getInt("roforbund.bruker", "Bruker_id", UID, "Rolle");
+
+
+        System.out.println(String.valueOf(fodseldato));
+        request.setAttribute("fodseldato", fodseldato);
+        request.setAttribute("epost", epost);
+        request.setAttribute("tlf", tlf);
+        request.setAttribute("gatenavn", gatenavn);
+        request.setAttribute("husnummer", husnummer);
+        request.setAttribute("poststed", poststed);
+        request.setAttribute("hoyde", hoyde);
+        request.setAttribute("vekt", vekt);
+        request.setAttribute("rolle", rolle);
+
+
+
+
+        RequestDispatcher rq = request.getRequestDispatcher("../MinProfil/index.jsp");
         rq.forward(request, response);
     }
 }
