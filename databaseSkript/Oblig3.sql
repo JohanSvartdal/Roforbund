a0a9285c9e37f6e7dae992a805259ae22047ff7c
Mari Tveiten Hoslemo <maritho@student.uia.no>
2020-11-13 13:18:02 +0100

    Lagt inn spørringerni oblig3, og litt usikker på om jeg har pusha det nyeste jeg la inn i databasen, men da kommer det her i såfall

--- databaseSkript/Oblig3.sql
+++ databaseSkript/Oblig3.sql
@@ -3,3 +3,24 @@ FROM bruker b
 INNER JOIN klubber k
 ON b.Klubb_id = k.Klubb_id
 WHERE Rolle = 2;
+
+
+SELECT * FROM bruker
+ORDER BY Bruker_id
+LIMIT 10;
+
+SELECT * FROM klubber
+ORDER BY Klubb_id
+LIMIT 10;
+
+SELECT * FROM ovelser
+ORDER BY Ovelse_id
+LIMIT 10;
+
+SELECT * FROM resultater
+ORDER BY Resultat_id
+LIMIT 10;
+
+SELECT * FROM tester
+ORDER BY Test_id
+LIMIT 10;
