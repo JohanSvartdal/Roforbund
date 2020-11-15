7ef5720b61952c38500c0db99befac0e004dd768
Johan Svartdal <johans@student.uia.no>
2020-11-15 18:28:03 +0100

    La til en popup boks på første side bare som en test. Denne må fjernes senere

--- src/main/webapp/Template.js
+++ src/main/webapp/Template.js
@@ -1,3 +1,15 @@
 (function() {
     //SKRIV JAVASCRIPT KODE HER
+
+    const knapp = document.getElementById("showPopup");
+
+    knapp.onclick = function() {
+        if (confirm("Press a button!")) {
+            //her trykkes OK
+            //Linjen under tar deg videre til google hvis du fjerner de to skråstrekene
+            //window.location.href = "www.google.com";
+        } else {
+            //her trykkes cancel
+        }
+    }
 }());
