18facbb9d60b972cfe25592c96b973708a2ae454
Johan Svartdal <johans@student.uia.no>
2020-12-11 20:56:06 +0100

    La til databasetingene igjen

---
+++ src/main/java/tools/database/DatabaseValue.java
@@ -0,0 +1,26 @@
+package tools.database;
+
+import java.sql.Date;
+
+public class DatabaseValue {
+    String text = null;
+    Integer number = null;
+    Date date = null;
+    Boolean aBoolean = null;
+
+    public DatabaseValue(int number) {
+        this.number = number;
+    }
+
+    public DatabaseValue(String text) {
+        this.text = text;
+    }
+
+    public DatabaseValue(Date date) {
+        this.date = date;
+    }
+
+    public DatabaseValue(boolean bool) {
+        this.aBoolean = bool;
+    }
+}
