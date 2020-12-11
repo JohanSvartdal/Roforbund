4a4579003fd47cc2e319bd212af2355e3eacb48c
Mari Tveiten Hoslemo <maritho@student.uia.no>
2020-12-11 09:27:24 +0100

    Oblig3

--- databaseSkript/Oblig3.sql
+++ databaseSkript/Oblig3.sql
@@ -4,6 +4,20 @@ INNER JOIN klubber k
 ON b.Klubb_id = k.Klubb_id
 WHERE Rolle = 2;
 
+SELECT Fornavn, Etternavn, Navn
+FROM klubber k
+INNER JOIN bruker b
+ON k.Klubb_id = b.Klubb_id
+WHERE Rolle = 2;
+
+SELECT Tid, Watt, KG, Repetisjoner, Fornavn
+FROM bruker b
+INNER JOIN resultater r
+ON b.Bruker_id = r.Bruker_id
+WHERE Test_id = 1;
+
+
+
 
 SELECT * FROM bruker
 ORDER BY Bruker_id
@@ -24,3 +38,9 @@ LIMIT 10;
 SELECT * FROM tester
 ORDER BY Test_id
 LIMIT 10;
+
+SELECT Navn
+FROM klubber
+WHERE Klubb_id = 1;
+
+SELECT
